4853a79097868ac1e1b5e53dcba8997a838c49a9
Myron Chiu <Myron.gc@gmail.com>
2012-03-02 11:45:01 -0800

    hours better

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -14,18 +14,18 @@ $(document).ready(function(){
   };
 
   function minutes_ring () {
-    minutes.style[Modernizr.prefixed('transform')] = 'translateZ( -350px ) rotateY(' + -6 * time()/60 + 'deg)';
-  }
+    minutes.style[Modernizr.prefixed('transform')] = 'translateZ( -370px ) rotateY(' + -6 * time()/60 + 'deg)';
+  };
   function hours_ring () {
-    hours.style[Modernizr.prefixed('transform')] = 'translateZ( -300px ) rotateY(' + 15 * time()/3600 + 'deg)';
-  }
+    hours.style[Modernizr.prefixed('transform')] = 'translateZ( -300px ) rotateY(' + -15 * ((time()/3600)-8) + 'deg)';
+  };
   function set_clock(){
     seconds.style[Modernizr.prefixed('transform')] = 'rotateY('+ 0 +'deg)'
     minutes.style[Modernizr.prefixed('transform')] = 'rotateY('+ 10 +'deg)'
     hours.style[Modernizr.prefixed('transform')] = 'rotateY('+ 10 +'deg)'
     minutes_ring()
     hours_ring()
-  }
+  };
 
   set_clock();
   setInterval(seconds_ring, 1000);
