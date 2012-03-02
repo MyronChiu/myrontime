05c80632f1102424263fe54c9b679a7dd2578c55
Myron Chiu <Myron.gc@gmail.com>
2012-03-02 03:18:54 -0800

    wtf hours

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
     return now.getTime()/1000;
   }
 
+  function alpha (current_fig, fig_to_blend, max_fig) {
+    return 1.0 - abs(current_fig - fig_to_blend) / (max_fig/2)
+  }
+
   function seconds_ring(){
     seconds.style[Modernizr.prefixed('transform')] = 'translateZ( -400px ) rotateY(' + -6 * time() + 'deg)';
   };
@@ -13,7 +17,7 @@ $(document).ready(function(){
     minutes.style[Modernizr.prefixed('transform')] = 'translateZ( -350px ) rotateY(' + -6 * time()/60 + 'deg)';
   }
   function hours_ring () {
-    hours.style[Modernizr.prefixed('transform')] = 'translateZ( -200px ) rotateY(' + -6 * time()/3600 + 'deg)';
+    hours.style[Modernizr.prefixed('transform')] = 'translateZ( -300px ) rotateY(' + 15 * time()/3600 + 'deg)';
   }
   function set_clock(){
     seconds.style[Modernizr.prefixed('transform')] = 'rotateY('+ 0 +'deg)'
@@ -25,7 +29,7 @@ $(document).ready(function(){
 
   set_clock();
   setInterval(seconds_ring, 1000);
-  setInterval(minutes_ring, 1000);  
-  setInterval(hours_ring, 1000);  
+  setInterval(minutes_ring, 60000);  
+  setInterval(hours_ring, 3600000);  
 });
 
