763b90376ba4464523940ffa1e6371a3a97dfabb
Myron Chiu <Myron.gc@gmail.com>
2012-03-02 18:34:01 -0800

    rings

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -11,25 +11,26 @@ $(document).ready(function(){
 
   function seconds_ring(){
     seconds.style[Modernizr.prefixed('transform')] = 'translateZ( -400px ) rotateY(' + -6 * time() + 'deg)';
-  };
+  }
 
-  function minutes_ring () {
+  function minutes_ring() {
     minutes.style[Modernizr.prefixed('transform')] = 'translateZ( -370px ) rotateY(' + -6 * time()/60 + 'deg)';
-  };
-  function hours_ring () {
+  }
+  function hours_ring() {
     hours.style[Modernizr.prefixed('transform')] = 'translateZ( -300px ) rotateY(' + -15 * ((time()/3600)-8) + 'deg)';
-  };
+  }
   function set_clock(){
     seconds.style[Modernizr.prefixed('transform')] = 'rotateY('+ 0 +'deg)'
     minutes.style[Modernizr.prefixed('transform')] = 'rotateY('+ 10 +'deg)'
     hours.style[Modernizr.prefixed('transform')] = 'rotateY('+ 10 +'deg)'
-    minutes_ring()
-    hours_ring()
-  };
+    seconds_ring();
+    minutes_ring();
+    hours_ring();
+  }
 
   set_clock();
   setInterval(seconds_ring, 1000);
-  setInterval(minutes_ring, 60000);  
-  setInterval(hours_ring, 3600000);  
+  setInterval(minutes_ring, 1000);  
+  setInterval(hours_ring, 1000);  
 });
 
