3c7c38179525a9dd68d8c180ba4fee852e4de98e
Myron Chiu <Myron.gc@gmail.com>
2012-03-03 13:47:42 -0800

    clock good

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -5,39 +5,28 @@ $(document).ready(function(){
     return now.getTime()/1000;
   }
 
-  function alpha (current_fig, fig_to_blend, max_fig) {
-    return 1.0 - abs(current_fig - fig_to_blend) / (max_fig/2)
+  function highlighter (ring_section, time_interval) {
+    $($("#"+ring_section+" figure").filter("figure")).css("background-color","rgba(255, 255, 255, .6)").css("color","black")    
+    $($("#"+ring_section+" figure").filter("figure")[time_interval]).css("background-color","rgba(76, 76, 76, 1)").css("color","white")
   }
 
-  function testing_ring(){
-    testing.style[Modernizr.prefixed('transform')] = 'translateZ( -400px ) rotateY(' + -360 * time() + 'deg)';
+  function highlight () {
+    var sec = (parseInt(time())-1) % 60
+    var min = parseInt((time()-1)/60-1) % 60
+    var hrs = parseInt(((time()-1)/3600-1)-8) % 24
+    highlighter("minutes", min)
+    highlighter("hours", hrs)
+    highlighter("seconds", sec)
   }
 
-  function seconds_ring(){
-    seconds.style[Modernizr.prefixed('transform')] = 'translateZ( -400px ) rotateY(' + -6 * time() + 'deg)';
-  }
-
-  function minutes_ring () {
+  function rings(){ 
+    seconds.style[Modernizr.prefixed('transform')] = 'translateZ( -400px ) rotateY(' + -6 * parseInt(time()) + 'deg)';
     minutes.style[Modernizr.prefixed('transform')] = 'translateZ( -370px ) rotateY(' + -6 * parseInt(time()/60) + 'deg)';
-  }
-  function hours_ring () {
     hours.style[Modernizr.prefixed('transform')] = 'translateZ( -300px ) rotateY(' + -15 * parseInt(((time()/3600)-8)) + 'deg)';
+    testing.style[Modernizr.prefixed('transform')] = 'translateZ( -400px ) rotateY(' + -360 * parseInt(time()) + 'deg)';
   }
-  function set_clock(){
-    seconds.style[Modernizr.prefixed('transform')] = 'rotateY('+ 0 +'deg)'
-    minutes.style[Modernizr.prefixed('transform')] = 'rotateY('+ 10 +'deg)'
-    hours.style[Modernizr.prefixed('transform')] = 'rotateY('+ 10 +'deg)'
-    seconds_ring();
-    minutes_ring();
-    hours_ring();
-    testing_ring();
-  }
-
-  set_clock();
-  setInterval(seconds_ring, 1000);
-  setInterval(minutes_ring, 1000);  
-  setInterval(hours_ring, 1000);  
-  setInterval(testing_ring, 1000);  
+  setInterval(rings, 500);
+  setInterval(highlight, 500);
 
 });
 
