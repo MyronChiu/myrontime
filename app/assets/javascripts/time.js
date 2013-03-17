fd64b9772a382d16515f4647a40fdbb8ebfad31d
Myron Chiu <myron.gc@gmail.com>
2013-03-17 03:29:24 -0700

    goodness

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -1,23 +1,37 @@
 //
 $(document).ready(function(){
-
+  var sec_rotations = 0;
+  var min_rotations = 0;
+  var hrs_rotations = 0;
   function time () {
     var now = new Date().getTime()/1000;
     var sec = (parseInt(now)) % 60;
     var min = parseInt((now)/60) % 60;
     var hrs = parseInt(((now)/3600)-7) % 24;
+    var sec_nomod = -6 * parseInt(now)%360;
+    var min_nomod = -6 * parseInt((now)/60)%360;
+    var hrs_nomod = -15 * parseInt((((now)/3600)-7))%360;
+    
+    if(sec_nomod % 360 === 0){sec_rotations += 1};
+    if(sec_rotations > 0){sec_nomod -= 360*sec_rotations};
+    console.log(sec_rotations);
+    if(min_nomod % 360 === 0){min_rotations += 1};
+    if(min_rotations > 0){min_nomod -= 360*min_rotations}; 
+
+    if(hrs_nomod % 360 === 0){hrs_rotations += 1};
+    if(hrs_rotations > 0){hrs_nomod -= 360*hrs_rotations};
 
     highlighter("minutes", min);
     highlighter("hours", hrs);
     highlighter("seconds", sec);
 
-    sec_t = 'translateZ(-400px) rotateY(' + -6 * parseInt(now) + 'deg)';
-    min_t = 'translateZ( -370px ) rotateY(' + -6 * parseInt((now)/60) + 'deg)';
-    hrs_t = 'translateZ( -300px ) rotateY(' + -15 * parseInt((((now)/3600)-7)) + 'deg)';
+    sec_t = 'translateZ(-400px) rotateY(' + sec_nomod + 'deg)';
+    min_t = 'translateZ( -370px ) rotateY(' + min_nomod + 'deg)';
+    hrs_t = 'translateZ( -300px ) rotateY(' + hrs_nomod + 'deg)';
 
-    $("#seconds").css({"-webkit-transform" : sec_t});
-    $("#minutes").css({"-webkit-transform" : min_t});
-    $("#hours").css({"-webkit-transform" : hrs_t});
+    $("#seconds").css({"-webkit-transform" : sec_t,"-moz-transform" : sec_t});
+    $("#minutes").css({"-webkit-transform" : min_t,"-moz-transform" : min_t});
+    $("#hours").css({"-webkit-transform" : hrs_t,"-moz-transform" : hrs_t});
   }
 
   function highlighter (ring_section, time_interval) {
@@ -26,7 +40,7 @@ $(document).ready(function(){
     $(ring[time_interval]).addClass("highlight");
   }
 
-
+  time();
   setInterval(time, 1000);
 });
 
